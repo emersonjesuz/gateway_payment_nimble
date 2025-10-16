52766bec688f0e1ef30fb60a983a5442249b3b68
emersonjesuz <josiemerson2013@gmail.com>
2025-10-16 18:50:01 -0300

    chore: init project

---
+++ src/main/java/com/nimble/gateway_payment/GatewayPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.nimble.gateway_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimble/gateway_payment/GatewayPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimble.gateway_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
