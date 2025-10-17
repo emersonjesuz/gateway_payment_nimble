e5d90aaebde1bc00f9263921ca1e390088981b63
emersonjesuz <josiemerson2013@gmail.com>
2025-10-17 20:33:29 -0300

    feat: ensure not returns error in AuthUseCase if everything is correct

--- src/main/java/com/nimble/gateway_payment/auth/useCases/AuthUseCase.java
+++ src/main/java/com/nimble/gateway_payment/auth/useCases/AuthUseCase.java
@@ -1,20 +1,20 @@
 package com.nimble.gateway_payment.auth.useCases;
 
 import com.nimble.gateway_payment.auth.dtos.RegisterInputDto;
+import com.nimble.gateway_payment.user.entities.UserEntity;
 import com.nimble.gateway_payment.user.repositories.UserRepository;
 import com.nimble.gateway_payment.user.valueObjects.Cpf;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUseCase {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public AuthUseCase(UserRepository userRepository) {
+    public AuthUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void register(RegisterInputDto dto) {
@@ -22,5 +22,13 @@ public class AuthUseCase {
         if (this.userRepository.findByEmailOrCPF(dto.getEmail(), cpf.getValue()).isPresent()) {
             throw new IllegalArgumentException("User already exists");
         }
+        var passwordHash = this.passwordEncoder.encode(dto.getPassword());
+        UserEntity user = UserEntity.builder()
+                .email(dto.getEmail())
+                .cpf(cpf.getValue())
+                .name(dto.getName())
+                .password(passwordHash)
+                .build();
+        this.userRepository.save(user);
     }
 }
