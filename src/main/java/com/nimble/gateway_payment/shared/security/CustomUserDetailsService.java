3120df4be52d4180c8dde255cf07ed05d0accb77
emersonjesuz <josiemerson2013@gmail.com>
2025-10-17 10:00:47 -0300

    chore: add security config

---
+++ src/main/java/com/nimble/gateway_payment/shared/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.nimble.gateway_payment.shared.security;
+
+import com.nimble.gateway_payment.user.entities.UserEntity;
+import com.nimble.gateway_payment.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = this.repository.findByEmail(username).orElseThrow(() -> new RuntimeException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
---
+++ src/main/java/com/nimble/gateway_payment/shared/security/TokenService.java
@@ -0,0 +1,51 @@
+package com.nimble.gateway_payment.shared.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.nimble.gateway_payment.user.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserEntity user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(8).toInstant(ZoneOffset.of("-3"));
+    }
+}
