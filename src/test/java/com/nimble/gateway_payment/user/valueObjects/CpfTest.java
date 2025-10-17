05404f5f1b50d5a1d4bcf942d77a2160d2d1bf8d
emersonjesuz <josiemerson2013@gmail.com>
2025-10-17 11:43:07 -0300

    test: ensure return error if CPF Invalid

--- src/test/java/com/nimble/gateway_payment/user/valueObjects/CpfTest.java
+++ src/test/java/com/nimble/gateway_payment/user/valueObjects/CpfTest.java
@@ -7,23 +7,30 @@ import org.junit.jupiter.api.Test;
 public class CpfTest {
 
     @Test
-    public void shouldReturnAnErrorIfCpfIsNull (){
+    public void shouldReturnAnErrorIfCpfIsNull(){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new Cpf(null));
         assertEquals("The CPF cannot be null.", exception.getMessage());
     }
 
     @Test
-    public void shouldReturnAnErrorIfCPFHasInvalidCharacter (){
+    public void shouldReturnAnErrorIfCPFHasInvalidCharacter(){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new Cpf("111.111.111-30"));
         assertEquals("The CPF must contain only numbers.", exception.getMessage());
     }
 
     @Test
-    public void shouldReturnAnErrorIfCPFNot11NumericDigits (){
+    public void shouldReturnAnErrorIfCPFNot11NumericDigits(){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new Cpf("11111111"));
         assertEquals("The CPF must contain exactly 11 numeric digits.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldReturnAnErrorIfCPFInvalid(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Cpf("11111111111"));
+        assertEquals("CPF inválid.", exception.getMessage());
     }
 }
