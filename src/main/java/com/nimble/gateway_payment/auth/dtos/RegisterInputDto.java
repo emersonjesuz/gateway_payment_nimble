d1b512bd7b49a2e9ee583245b74448479127f2af
emersonjesuz <josiemerson2013@gmail.com>
2025-10-17 15:37:39 -0300

    feat: create RegisterInputDto

---
+++ src/main/java/com/nimble/gateway_payment/auth/dtos/RegisterInputDto.java
@@ -0,0 +1,26 @@
+package com.nimble.gateway_payment.auth.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterInputDto {
+    @NotBlank(message = "The name field cannot be empty")
+    private String name;
+    @NotBlank(message = "The email field cannot be empty")
+    @Email(message = "The email field cannot be invalid")
+    private String email;
+    @NotBlank(message = "The CPF field cannot be empty")
+    @CPF(message = "The CPF field cannot be invalid")
+    private String cpf;
+    @NotBlank(message = "The name field cannot be empty")
+    @Min(value = 6, message = "The Password field must have at least 6 digits")
+    private String password;
+}
