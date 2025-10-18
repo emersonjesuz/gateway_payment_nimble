3eb6f9e6a8f7f19d533483dba55b9c4b3c32da53
emersonjesuz <josiemerson2013@gmail.com>
2025-10-18 17:34:30 -0300

    feat: ensure method getEmail return null if identify is not an email

--- src/main/java/com/nimble/gateway_payment/auth/IdentifierValidator.java
+++ src/main/java/com/nimble/gateway_payment/auth/IdentifierValidator.java
@@ -1,9 +1,20 @@
 package com.nimble.gateway_payment.auth;
 
+import lombok.Getter;
+
+@Getter
 public class IdentifierValidator {
+    private String email;
+
     public IdentifierValidator(String value) {
         if (value == null || value.isEmpty()) {
             throw new IllegalArgumentException("Identifier is not empty.");
         }
+
+        if (value.contains("@") && value.contains(".")) {
+            this.email = value;
+            return;
+        }
     }
+
 }
