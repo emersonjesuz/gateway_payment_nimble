d2fb6abe313e8659128dd6708baf772dba37ccf6
emersonjesuz <josiemerson2013@gmail.com>
2025-10-17 23:10:28 -0300

    test: ensure return 400 if password field has invalid password

--- src/test/java/com/nimble/gateway_payment/auth/AuthControllerTest.java
+++ src/test/java/com/nimble/gateway_payment/auth/AuthControllerTest.java
@@ -77,4 +77,18 @@ public class AuthControllerTest {
         assertEquals(400, response.status());
         assertEquals("The CPF field cannot be invalid", response.message());
     }
+
+    @Test
+    public void shouldReturn400IfCpfInvalid() {
+        String cpfWith12Digits = "123456789012";
+        RegisterInputDto dto = RegisterInputDto.builder()
+                .name("josi")
+                .email("josi@email.com")
+                .cpf(cpfWith12Digits)
+                .password("password1234")
+                .build();
+        ErrorResponse response = this.restTemplate.postForObject("/auth/register", dto, ErrorResponse.class);
+        assertEquals(400, response.status());
+        assertEquals("The CPF field cannot be invalid", response.message());
+    }
 }
