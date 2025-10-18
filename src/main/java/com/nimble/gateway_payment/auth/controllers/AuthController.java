fa2fe4dc017d75dc248e85981644aab1b013a93f
emersonjesuz <josiemerson2013@gmail.com>
2025-10-17 21:12:01 -0300

    feat: add file RegisterOutputDto and AuthController

---
+++ src/main/java/com/nimble/gateway_payment/auth/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.nimble.gateway_payment.auth.controllers;
+
+import com.nimble.gateway_payment.auth.dtos.RegisterInputDto;
+import com.nimble.gateway_payment.auth.dtos.RegisterOutputDto;
+import com.nimble.gateway_payment.auth.useCases.AuthUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthUseCase authUseCase;
+
+    public AuthController(AuthUseCase authUseCase) {
+        this.authUseCase = authUseCase;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterOutputDto> register(@RequestBody @Valid RegisterInputDto body) {
+        this.authUseCase.register(body);
+        return ResponseEntity.status(200).body(new RegisterOutputDto("User created with success."));
+    }
+}
---
+++ src/main/java/com/nimble/gateway_payment/auth/dtos/RegisterOutputDto.java
@@ -0,0 +1,4 @@
+package com.nimble.gateway_payment.auth.dtos;
+
+public record RegisterOutputDto(String message) {
+}
