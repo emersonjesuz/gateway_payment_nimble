c21e83d5d0582795e3a5cc8005faf50c6e6477c1
emersonjesuz <josiemerson2013@gmail.com>
2025-10-18 12:40:37 -0300

    chore: add database h2database to test

--- src/test/java/com/nimble/gateway_payment/auth/AuthControllerTest.java
+++ src/test/java/com/nimble/gateway_payment/auth/AuthControllerTest.java
@@ -9,10 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class AuthControllerTest {
 
     @Autowired
