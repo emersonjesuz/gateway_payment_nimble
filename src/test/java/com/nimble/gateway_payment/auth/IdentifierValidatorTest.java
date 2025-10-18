928ff1055f451ade349e7d31832d349fea565119
emersonjesuz <josiemerson2013@gmail.com>
2025-10-18 17:14:38 -0300

    test: ensure return error if identifier is empty

---
+++ src/test/java/com/nimble/gateway_payment/auth/IdentifierValidatorTest.java
@@ -0,0 +1,16 @@
+package com.nimble.gateway_payment.auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IdentifierValidatorTest {
+
+    @Test
+    public void shouldReturnErrorIfIdentifierIsEmpty() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new IdentifierValidator(""));
+        assertEquals("Identifier is not empty.", exception.getMessage());
+    }
+}
