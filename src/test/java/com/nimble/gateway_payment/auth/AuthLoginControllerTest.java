246be060212d51ce78450798b873731e081b5559
emersonjesuz <josiemerson2013@gmail.com>
2025-10-20 11:37:15 -0300

    test: ensure return 400 if password incorrect

--- src/test/java/com/nimble/gateway_payment/auth/AuthLoginControllerTest.java
+++ src/test/java/com/nimble/gateway_payment/auth/AuthLoginControllerTest.java
@@ -2,6 +2,8 @@ package com.nimble.gateway_payment.auth;
 
 import com.nimble.gateway_payment.TestUtils;
 import com.nimble.gateway_payment.auth.dtos.LoginInputDto;
+import com.nimble.gateway_payment.auth.dtos.RegisterInputDto;
+import com.nimble.gateway_payment.user.UserEntity;
 import com.nimble.gateway_payment.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,11 +37,21 @@ public class AuthLoginControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @BeforeEach
     public void setup() {
         this.mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .build();
+    }
+
+    private void createUser(RegisterInputDto dto) {
+        var passwordHash = this.passwordEncoder.encode(dto.getPassword());
+        dto.setPassword(passwordHash);
+        var user = new UserEntity(dto);
+        this.userRepository.saveAndFlush(user);
     }
 
     @Test
@@ -116,6 +129,27 @@ public class AuthLoginControllerTest {
     public void shouldReturn400IfIdentifierByCpfNotExists() throws Exception {
         LoginInputDto dto = LoginInputDto.builder()
                 .identifier("12233344540")
+                .password("123456").build();
+        this.mvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.objectToJSON(dto))
+                )
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Identifier or password incorrect."));
+    }
+
+    @Test
+    public void shouldReturn400IfPasswordIncorrect() throws Exception {
+        RegisterInputDto registerDto = RegisterInputDto.builder()
+                .name("josi")
+                .email("josi1@email.com")
+                .cpf("38485789300")
+                .password("passwordIncorrect")
+                .build();
+        this.createUser(registerDto);
+
+        LoginInputDto dto = LoginInputDto.builder()
+                .identifier("josi1@email.com")
                 .password("123456").build();
         this.mvc.perform(post("/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
