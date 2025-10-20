26bcf327db2aee9950c9803a389f6662ef9aa9e2
emersonjesuz <josiemerson2013@gmail.com>
2025-10-20 15:58:42 -0300

    test: ensure return error if recipientCpf not contain exactly 11 numeric digits

--- src/test/java/com/nimble/gateway_payment/charge/ChargeCreateUseCasesTest.java
+++ src/test/java/com/nimble/gateway_payment/charge/ChargeCreateUseCasesTest.java
@@ -5,6 +5,7 @@ import com.nimble.gateway_payment.charges.ChargeUseCase;
 import com.nimble.gateway_payment.charges.dtos.ChargeCreateInputDto;
 import com.nimble.gateway_payment.user.UserRepository;
 import com.nimble.gateway_payment.user.exceptions.CpfCannotBeNullException;
+import com.nimble.gateway_payment.user.exceptions.CpfMustContainExactly11NumbersException;
 import com.nimble.gateway_payment.user.exceptions.CpfMustContainOnlyNumbersException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,5 +44,14 @@ public class ChargeCreateUseCasesTest {
             this.chargeUseCase.create(dto);
         });
         assertEquals("The CPF must contain only numbers.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldReturnAnErrorIfRecipientCpfNot11NumericDigits() {
+        ChargeCreateInputDto dto = ChargeCreateInputDto.builder().recipientCpf("111222333445").build();
+        CpfMustContainExactly11NumbersException exception = assertThrows(CpfMustContainExactly11NumbersException.class, () -> {
+            this.chargeUseCase.create(dto);
+        });
+        assertEquals("The CPF must contain exactly 11 numeric digits.", exception.getMessage());
     }
 }
