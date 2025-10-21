78ecb40f438bd246588585f7161fc7b787a7ef59
emersonjesuz <josiemerson2013@gmail.com>
2025-10-21 13:14:48 -0300

    refactor: change validation  annotation NotBlank to NotNull in amount

--- src/main/java/com/nimble/gateway_payment/charges/dtos/ChargeCreateInputDto.java
+++ src/main/java/com/nimble/gateway_payment/charges/dtos/ChargeCreateInputDto.java
@@ -3,6 +3,7 @@ package com.nimble.gateway_payment.charges.dtos;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,7 +21,7 @@ public class ChargeCreateInputDto {
     @CPF(message = "The recipientCpf field cannot be invalid")
     private String recipientCpf;
 
-    @NotBlank(message = "The amount field cannot be empty")
+    @NotNull(message = "The amount field cannot be empty")
     @DecimalMin(value = "0.01", inclusive = true, message = "Amount must be greater than 0")
     @DecimalMax(value = "10000.00", inclusive = true, message = "Amount must not exceed R$ 10,000")
     private BigDecimal amount;
