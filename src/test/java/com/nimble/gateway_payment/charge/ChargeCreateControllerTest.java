596718eca6314b9989cd9bf709efccc05dbe644f
emersonjesuz <josiemerson2013@gmail.com>
2025-10-21 19:48:00 -0300

    test: ensure return error if not exists user with recipientCpf

--- src/test/java/com/nimble/gateway_payment/charge/ChargeCreateControllerTest.java
+++ src/test/java/com/nimble/gateway_payment/charge/ChargeCreateControllerTest.java
@@ -178,4 +178,24 @@ public class ChargeCreateControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("The CPF must contain only numbers."));
     }
+
+    @Test
+    public void shouldReturn400IfExistsRecipientUserWithCpf() throws Exception {
+        ChargeCreateInputDto dto = ChargeCreateInputDto.builder().recipientCpf("38485789300").amount(BigDecimal.valueOf(100)).build();
+        RegisterInputDto registerDto = RegisterInputDto.builder()
+                .name("josi")
+                .email("josi1@email.com")
+                .cpf("11501002902")
+                .password("123456")
+                .build();
+        this.createUser(registerDto);
+        this.mvc.perform(post("/charge")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.objectToJSON(dto))
+                        .cookie(new Cookie("userId", this.userMock.getId().toString()))
+                        .header("Authorization", TestUtils.generatedToken(this.userMock))
+                )
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("User not found."));
+    }
 }
