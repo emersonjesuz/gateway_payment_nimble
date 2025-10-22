a100d53326191d85e0dbc4348d22ca7c9d1bb606
emersonjesuz <josiemerson2013@gmail.com>
2025-10-22 08:25:18 -0300

    feat: add method findAllByOriginatorUser in ChargeRepository

--- src/main/java/com/nimble/gateway_payment/charges/ChargeRepository.java
+++ src/main/java/com/nimble/gateway_payment/charges/ChargeRepository.java
@@ -1,10 +1,16 @@
 package com.nimble.gateway_payment.charges;
 
+import com.nimble.gateway_payment.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ChargeRepository extends JpaRepository<ChargeEntity, UUID> {
+    @Query("SELECT c FROM ChargeEntity c WHERE c.originatorUser = :user AND (:status IS NULL OR c.status = :status)")
+    List<ChargeEntity> findAllByOriginatorUser(@Param("user") UserEntity user, @Param("status") Status status);
 }
