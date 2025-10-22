d20a10f937224036371469afae94554de3af54b0
emersonjesuz <josiemerson2013@gmail.com>
2025-10-22 10:46:54 -0300

    feat: add method findAllByOriginator in ChargeController

--- src/main/java/com/nimble/gateway_payment/charges/ChargeController.java
+++ src/main/java/com/nimble/gateway_payment/charges/ChargeController.java
@@ -2,10 +2,13 @@ package com.nimble.gateway_payment.charges;
 
 import com.nimble.gateway_payment.charges.dtos.ChargeCreateInputDto;
 import com.nimble.gateway_payment.charges.dtos.ChargeCreateOutputDto;
+import com.nimble.gateway_payment.charges.dtos.ChargeOutputDto;
 import jakarta.validation.Valid;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,5 +25,11 @@ public class ChargeController {
     private ResponseEntity<ChargeCreateOutputDto> create(@RequestBody @Valid ChargeCreateInputDto body, @CookieValue(name = "userId") String userId) {
         this.chargeUseCase.create(body, UUID.fromString(userId));
         return ResponseEntity.status(200).body(new ChargeCreateOutputDto("Charge created with success."));
+    }
+
+    @GetMapping("/created")
+    private ResponseEntity<List<ChargeOutputDto>> findAllByOriginator(@Param("status") Status status, @CookieValue(name = "userId") String userId) {
+        var result = this.chargeUseCase.findAllCreateCharge(status, UUID.fromString(userId));
+        return ResponseEntity.status(200).body(result);
     }
 }
