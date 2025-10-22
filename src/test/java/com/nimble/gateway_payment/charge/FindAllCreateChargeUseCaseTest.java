eabeee630efd6ac8367f7220f86453d15454acf0
emersonjesuz <josiemerson2013@gmail.com>
2025-10-22 09:58:25 -0300

    test: ensure return list empty if not exist charges

--- src/test/java/com/nimble/gateway_payment/charge/FindAllCreateChargeUseCaseTest.java
+++ src/test/java/com/nimble/gateway_payment/charge/FindAllCreateChargeUseCaseTest.java
@@ -1,8 +1,10 @@
 package com.nimble.gateway_payment.charge;
 
+import com.nimble.gateway_payment.charges.ChargeEntity;
 import com.nimble.gateway_payment.charges.ChargeRepository;
 import com.nimble.gateway_payment.charges.ChargeUseCase;
 import com.nimble.gateway_payment.charges.Status;
+import com.nimble.gateway_payment.user.UserEntity;
 import com.nimble.gateway_payment.user.UserRepository;
 import com.nimble.gateway_payment.user.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.Test;
@@ -11,10 +13,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class FindAllCreateChargeUseCaseTest {
@@ -35,5 +41,15 @@ public class FindAllCreateChargeUseCaseTest {
             this.chargeUseCase.findAllCreateCharge(Status.PENDING, originatorId);
         });
         assertEquals("User not found.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldReturnListEmptyIfNotExistCharges() {
+        UserEntity user = UserEntity.builder().build();
+        when(this.userRepository.findById(any())).thenReturn(Optional.of(user));
+        List<ChargeEntity> listMock = new ArrayList<>();
+        when(this.chargeRepository.findAllByOriginatorUser(any(), any())).thenReturn(listMock);
+        var result = this.chargeUseCase.findAllCreateCharge(Status.PENDING, originatorId);
+        assertTrue(result.isEmpty());
     }
 }
