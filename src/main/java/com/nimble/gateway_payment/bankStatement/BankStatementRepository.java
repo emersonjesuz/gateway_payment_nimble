83058800c683cc8052a23dc1fc87c724e0fdebaf
emersonjesuz <josiemerson2013@gmail.com>
2025-10-23 11:03:34 -0300

    feat: add BankStatementRepository

---
+++ src/main/java/com/nimble/gateway_payment/bankStatement/BankStatementRepository.java
@@ -0,0 +1,13 @@
+package com.nimble.gateway_payment.bankStatement;
+
+import com.nimble.gateway_payment.charges.ChargeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BankStatementRepository extends JpaRepository<BankStatementEntity, UUID> {
+    Optional<BankStatementEntity> findByCharges(ChargeEntity charge);
+}
